0a10b77a73072223e8a72a50fad00841a67494d3
Jason Duque <duque.jason24@gmail.com>
2025-06-23 16:20:51 -0600

    updating stuff

---
+++ DietAppMain.java
@@ -0,0 +1,12 @@
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+
+public class DietAppMain {
+}
